fdac726f2cad3f44ec841a6c80d8566468ea761e
zarkonow <stojanov1987@gmail.com>
2025-06-09 01:36:50 +0200

    feat: Implement header, task management components, and user interface
    - Added HeaderComponent with styling and layout for the application header.
    - Created CardComponent for reusable card layout.
    - Developed NewTaskComponent for adding new tasks with form handling.
    - Implemented NoTaskComponent to display a message when no tasks are available.
    - Built TaskComponent to display individual tasks with completion functionality.
    - Established TasksComponent to manage and display a list of tasks.
    - Created UserTasksComponent for user-specific task management.
    - Developed UserComponent to display user information with avatar.
    - Implemented UsersComponent to list all users.
    - Added UsersService to manage user data.
    - Introduced dummy user data for testing and development.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UsersComponent],
 })
-export class AppComponent {
-  title = 'routing';
-}
+export class AppComponent {}
