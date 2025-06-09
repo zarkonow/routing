ca66e962c33e390eb30141d9545845f3674d9c52
zarkonow <stojanov1987@gmail.com>
2025-06-09 02:43:30 +0200

    feat: Configure routing for tasks component in main application

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
+import { TasksComponent } from './app/tasks/tasks.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter([{
+        path: 'tasks',
+        component: TasksComponent
+    },
+]),
+],
+}).catch((err) => console.error(err));
